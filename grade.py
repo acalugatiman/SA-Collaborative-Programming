c1fb3966db2f97e442ade7a3339a1ef9c301fd23
acalugatiman <acalugatiman@cmc.pshs.edu.ph>
2026-04-22 13:29:55 +0800

    Add files via upload

---
+++ grade.py
@@ -0,0 +1,11 @@
+q1 = float(input("Enter the tentative grade for Q1: "))
+q2 = float(input("Enter the tentative grade for Q2: "))
+q3 = float(input("Enter the tentative grade for Q3: "))
+q4 = float(input("Enter the tentative grade for Q4: "))
+
+Q1 = q1
+Q2 = (q1 + 2 * q2) / 3
+Q3 = (q2 + 2 * q3) / 3
+final_grade = (q3 + 2 * q4) / 3
+
+print(f"Final Grade: {final_grade}")
