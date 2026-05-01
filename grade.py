834819f84132fb91c4218f6c6e1859240898689f
KenzoRafaelManalo2026 <krmanalo@cmc.pshs.edu.ph>
2026-05-01 11:36:01 +0800

    Update grade.py

--- grade.py
+++ grade.py
@@ -8,5 +8,20 @@ Q2 = (q1 + (2 * q2)) / 3
 Q3 = (q2 + (2 * q3)) /3
 final_grade = (q3 + (2 * q4)) / 3
 
-print(f"Final Grade: {final_grade}")
+final_grade = int(final_grade * 100) / 100
+print(f"Final Grade: {final_grade:.2f}")
 
+if 1.00 <= final_grade <= 1.25:
+    print("EXCELLENT")
+elif 1.25 < final_grade <= 1.50:
+    print("VERY GOOD")
+elif 1.50 < final_grade <= 2.00:
+    print("GOOD")
+elif 2.00 < final_grade <= 2.50:
+    print("SATISFACTORY")
+elif 2.50 < final_grade <= 3.00:
+    print("FAIR")
+elif 3.00 < final_grade <= 4.00:
+    print("FAILED ON CONDITION")
+elif 4.00 < final_grade <= 5.00:
+    print("FAILED")
