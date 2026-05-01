a7f647d33f024b7d89b954479205b85b3fb2b90a
KenzoRafaelManalo2026 <krmanalo@cmc.pshs.edu.ph>
2026-05-01 13:00:44 +0800

    Update grade.py

--- grade.py
+++ grade.py
@@ -11,17 +11,25 @@ final_grade = (q3 + (2 * q4)) / 3
 final_grade = int(final_grade * 100) / 100
 print(f"Final Grade: {final_grade:.2f}")
 
-if 1.00 <= final_grade <= 1.25:
-    print("EXCELLENT")
-elif 1.25 < final_grade <= 1.50:
-    print("VERY GOOD")
-elif 1.50 < final_grade <= 2.00:
-    print("GOOD")
-elif 2.00 < final_grade <= 2.50:
-    print("SATISFACTORY")
-elif 2.50 < final_grade <= 3.00:
-    print("FAIR")
-elif 3.00 < final_grade <= 4.00:
-    print("FAILED ON CONDITION")
-elif 4.00 < final_grade <= 5.00:
-    print("FAILED")
+if 1.00 <= final_grade <= 1.125:
+    print("Final Grade: 1.00, EXCELLENT") 
+elif 1.126 < final_grade <= 1.375:
+    print("Final Grade: 1.25, VERY GOOD")
+elif 1.376 < final_grade <= 1.625:
+    print("Final Grade: 1.50, VERY GOOD")
+elif 1.626 < final_grade <= 1.875:
+    print("Final Grade: 1.75, GOOD")
+elif 1.876 < final_grade <= 2.125:
+    print("Final Grade: 2.00, GOOD")
+elif 2.126 < final_grade <= 2.375:
+    print("Final Grade: 2.25, SATISFACTORY")
+elif 2.376 < final_grade <= 2.625:
+    print("Final Grade: 2.50, SATISFACTORY")
+elif 2.626 < final_grade <= 2.875:
+    print("Final Grade: 2.75, FAIR")
+elif 2.876 < final_grade <= 3.00:
+    print("Final Grade: 3.00, FAIR")
+elif 3.01 < final_grade <= 4.00:
+    print("Final Grade: 4.00, FAILED ON CONDITION")
+elif 4.01 < final_grade <= 5.00:
+    print("Final Grade: 5.00, FAILED")
